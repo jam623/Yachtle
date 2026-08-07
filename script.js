abbadf959876694aecd17fcb77c3b9fca77df305
Nick Adames <njaadames@gmail.com>
2026-08-07 17:28:42 -0400

    Added favicon

--- script.js
+++ script.js
@@ -770,7 +770,7 @@ function copyShareScore() {
     const shareSummaryText = document.getElementById("shareSummaryText");
     if (!shareSummaryText) return;
 
-    const shareText = `${shareSummaryText.textContent}\nhttps://jam623.github.io/Yachtle/`;
+    const shareText = `${shareSummaryText.textContent}\nhttp://yachtlegame.com/`;
 
     navigator.clipboard.writeText(shareText).then(() => {
         const shareBtn = document.getElementById("shareScoreBtn");
